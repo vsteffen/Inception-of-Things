adb3a7da3fcc3620436d033ca8c3194fe26e5691
vboxuser <vboxuser@Ubuntu42.myguest.virtualbox.org>
2023-04-10 22:26:31 +0200

    feat: add install script for VirtualBox provider and Ansible

--- install.sh
+++ install.sh
@@ -1,5 +1,12 @@
-# See instructions at https://developer.hashicorp.com/vagrant/downloads
+#!/bin/bash
 
-wget -O- https://apt.releases.hashicorp.com/gpg | gpg --dearmor | sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg
-echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
-sudo apt update && sudo apt install vagrant
+# See instructions at https://developer.hashicorp.com/vagrant/downloads
+wget -O- https://apt.releases.hashicorp.com/gpg | gpg --dearmor | tee /usr/share/keyrings/hashicorp-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | tee /etc/apt/sources.list.d/hashicorp.list
+apt-get update && apt-get install vagrant
+
+# Install provider VirtualBox
+apt-get install -y virtualbox
+
+# Install Ansible
+apt-get install -y ansible
